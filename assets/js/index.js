d688a2dcf929a1a972e1bd13652b088d1937e6b5
DESKTOP-7CI91VE\Abdulrahman Alaa <abdelraman.bashir@vodafone.com>
2018-04-13 20:38:20 +0200

    adding auto update for cache changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,21 +5,26 @@ $(function () {
         logEvent('Window Load');
     }
 
-    window.applicationCache.onchecking = function () {
-        logEvent('Checking Cache');
-    }
-    window.applicationCache.ondownloading = function () {
-        logEvent('Downloading');
-    }
-    window.applicationCache.onnoupdate = function () {
-        logEvent('No Update');        
-    }
-    window.applicationCache.onupdateready = function (e) {
-        logEvent('Update Ready');
-        logEvent('Swapping Cache');
-        window.applicationCache.swapCache();
-    }
     function logEvent(text) {
         $('#log').append($('<li>' + text + '</li>'))
+    }
+
+    if (window.applicationCache) {
+        window.applicationCache.onchecking = function () {
+            logEvent('Checking Cache');
+        }
+        window.applicationCache.ondownloading = function () {
+            logEvent('Downloading');
+        }
+        window.applicationCache.onnoupdate = function () {
+            logEvent('No Update');
+        }
+        window.applicationCache.onupdateready = function (e) {
+            logEvent('Update Ready');
+            logEvent('Swapping Cache');
+            window.applicationCache.swapCache();
+            window.location.reload();
+        }
+       
     }
 });
