32d5ce21f62df369ecdab9a7011a0171c69ce61d
DESKTOP-7CI91VE\Abdulrahman Alaa <abdelraman.bashir@vodafone.com>
2018-04-14 03:10:35 +0200

    make db for remote storage and updating the cache handler

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,23 +1,71 @@
 $(function () {
-    function onOnline() {
-        reportOnlineStatus();
-        sendMessagesToServer()
+    var status = $('#status');
+    var messages = [];
+
+    function isOnline() {
+        return window && window.navigator && window.navigator.onLine;
     }
+
+    function onOnline() {
+        // Update html text
+        reportOnlineStatus();
+
+        // Send cached data to the server 
+        sendMessagesToServer();
+    }
+
     function onOffline() {
+        // Update html text
         reportOfflineStatus();
     }
 
     function sendMessagesToServer() {
-
+        messages.map(function (item) {
+            $.ajax(
+                {
+                    method: 'POST',
+                    url: 'assets/db.json',
+                    data: { id: Math.random() * 10, message: item }
+                }
+            );
+        });
+        messages = [];
     }
 
     function reportOnlineStatus() {
-
+        status.text('Online');
     }
 
     function reportOfflineStatus() {
-
+        status.text('Offline');
     }
+
+    function onSendClick() {
+        var val = $('#txtArea').val();
+        if (isOnline()) {
+            storeMessageRemote(val);
+        } else {
+            storeMessageLocal(val);
+        }
+        $('#txtArea').val('')
+    }
+
+    function storeMessageLocal(msg) {
+        messages.push(msg);
+    }
+
+    function storeMessageRemote(msg) {
+        $.ajax(
+            {
+                method: 'POST',
+                url: 'assets/db.json',
+                data: { id: Math.random() * 10, message: msg }
+            }
+        );
+    }
+
+    $('#btnSend').click(onSendClick);
     window.addEventListener('online', onOnline);
     window.addEventListener('offline', onOffline, true);
+    reportOnlineStatus();
 });
